b87ada4f00d0fe980be03867c4c4e2cd16ef6a2c
imasheshk <imasheshk@kluniversity.in>
2024-12-14 13:50:56 +0530

    Email verification implemented & Account numbers generation

--- Online_Banking/src/main/java/com/bank/demo/repository/TransactionRepository.java
+++ Online_Banking/src/main/java/com/bank/demo/repository/TransactionRepository.java
@@ -1,11 +1,9 @@
 package com.bank.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.bank.demo.entity.Transaction;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByUserId(Long userId);
+    List<Transaction> findByUserAccountNumber(String accountNumber);
 }
